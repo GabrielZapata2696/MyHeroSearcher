c039d761bd4c225905aefc1e20c415644f3c2ea1
Gabriel Zapata <sr.gabrielzm@gmail.com>
2024-01-18 11:44:17 -0500

    estructura heroPage y variables de entorno para futuras implementaciones y ampliaciones

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -1,3 +1,5 @@
 export const environments = {
-  baseUrl: 'https://localhost:3000'
+  baseUrl: 'https://localhost:3000',
+  baseUrlGeneral: `https://superheroapi.com/api/`,
+  accessToken: `Access token`
 }
--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,3 +1,5 @@
 export const environments = {
-  baseUrl: 'http://localhost:3000'
+  baseUrl: 'http://localhost:3000',
+  baseUrlGeneral: `https://superheroapi.com/api/`,
+  accessToken: `Access token`
 }
