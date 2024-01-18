399ca12d1fc014d7a9c44710156e8ddc8f12921e
Gabriel Zapata <sr.gabrielzm@gmail.com>
2024-01-18 11:59:53 -0500

    ajuste git ignore

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -1,5 +1,5 @@
 export const environments = {
   baseUrl: 'https://localhost:3000',
   baseUrlGeneral: `https://superheroapi.com/api/`,
-  accessToken: `Access token`
+  accessToken: `Access`
 }
--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,5 +1,5 @@
 export const environments = {
   baseUrl: 'http://localhost:3000',
   baseUrlGeneral: `https://superheroapi.com/api/`,
-  accessToken: `Access token`
+  accessToken: `7055320064544722`
 }
